4292279ce9861d884d714e9c63533fcf69dffb82
Emmanuel Obagunwa <access2emma@gmail.com>
2023-03-22 16:24:07 +0100

    saved changes

--- Connections/local_temp.php
+++ Connections/local_temp.php
@@ -1,14 +1,14 @@
 <?php
-// $hostname_local = "localhost";
-// $database_local = "id17437081_cbt";
-// $username_local = "id17437081_oobcuser";
-// $password_local = "jDjgi[4Xjz>F[-(!";
+$hostname_local = "localhost";
+$database_local = "id17437081_cbt";
+$username_local = "id17437081_oobcuser";
+$password_local = "jDjgi[4Xjz>F[-(!";
 ini_set ('display_errors', 1);
 
-$hostname_local = "localhost";
-$database_local = "";
-$username_local = "root";
-$password_local = "";
+// $hostname_local = "localhost";
+// $database_local = "";
+// $username_local = "root";
+// $password_local = "";
 
 $local = mysqli_connect($hostname_local, $username_local, $password_local, $database_local);
 
